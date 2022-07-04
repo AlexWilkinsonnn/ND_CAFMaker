f0a8c6cae1d4b8611baf31b88f0ee344299760fd
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-07-04 04:49:30 -0500

    backup scripts

---
+++ scripts/setup_edepsim.sh
@@ -0,0 +1,9 @@
+# edep-sim needs to know where a certain GEANT .cmake file is...
+G4_cmake_file=`find ${GEANT4_FQ_DIR}/lib64 -name 'Geant4Config.cmake'`
+export Geant4_DIR=`dirname $G4_cmake_file`
+
+# edep-sim needs to have the GEANT bin directory in the path
+export PATH=$PATH:$GEANT4_FQ_DIR/bin
+
+export LD_LIBRARY_PATH=${PWD}/edep-sim/edep-gcc-6.4.0-x86_64-pc-linux-gnu/lib:${LD_LIBRARY_PATH}
+export PATH=${PWD}/edep-sim/edep-gcc-6.4.0-x86_64-pc-linux-gnu/bin:${PATH}
